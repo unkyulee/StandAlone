b8f69b41e7416f7e5cdf50898384fd1afdb199e4
Un Kyu Lee <unkyulee@gmail.com>
2020-12-05 23:15:11 +0100

    recipe module

--- module/recipe/detail/component.js
+++ module/recipe/detail/component.js
@@ -1,51 +1,57 @@
 // _id: recipe-detail
 new Object({
   template: `
-  <div :style="style">
-  <v-breadcrumbs :items="breadcrumbs"></v-breadcrumbs>
-  <v-row align="center" justify="center" style="color: white; max-width: 800px; align-self: center;">
-    <v-col
-      class="text-center"
-      cols="12"
-      style="background: black; opacity: 0.8"
-    >
-      <h1 class="display-1 font-weight-bold mb-4">{{recipe.name}}</h1>
-      <h4 class="font-weight-thin">{{recipe.description}}</h4>
-      <v-img v-if="recipe.image" height="250" :src="recipe.image"></v-img>
-    </v-col>
-    <v-col cols="12">
-      <v-banner>Ingredienti</v-banner>
-      <v-simple-table>
-        <template v-slot:default>
-          <tbody>
-            <tr v-for="(item, index) of ingredients" :key="index">
-              <td>{{ item.name }}</td>
-              <td>{{ item.qty }}{{ item.unit }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
-    </v-col>
-    <v-col cols="12">
-      <v-banner>Procedimento</v-banner>
-      <v-simple-table>
-        <template v-slot:default>
-          <tbody>
-            <tr v-for="(item, index) of steps" :key="index">
-              <td>{{ item.description }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
-    </v-col>
-  </v-row>
-</div>
+<v-dialog v-model="show" max-width="600px" width="600px">
+  <v-img v-if="recipe.image" height="250" :src="recipe.image"></v-img>
 
+  <div style="background: white; padding: 16px;">
+    <h1 class="display-1 font-weight-bold mb-4">{{recipe.name}}</h1>
+    <h4 class="font-weight-thin">{{recipe.description}}</h4>
+
+    <v-banner style="font-weight: bold">Ingredienti</v-banner>
+
+    <v-row
+      align="center"
+      justify="center"
+      v-if="!ingredients"
+      style="height: 40px"
+    >
+      <v-progress-circular indeterminate color="primary" />
+    </v-row>
+
+    <v-simple-table>
+      <template v-slot:default>
+        <tbody>
+          <tr v-for="(item, index) of ingredients" :key="index">
+            <td>{{ item.name }}</td>
+            <td>{{ item.qty }}{{ item.unit }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+
+    <v-banner style="font-weight: bold">Procedimento</v-banner>
+
+    <v-row align="center" justify="center" v-if="!steps" style="height: 40px">
+      <v-progress-circular indeterminate color="primary" />
+    </v-row>
+
+    <v-simple-table>
+      <template v-slot:default>
+        <tbody>
+          <tr v-for="(item, index) of steps" :key="index">
+            <td>{{ item.description }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+</v-dialog>
   `,
   inject: ["data", "config", "event"],
   data: function () {
     return {
-      breadcrumbs: [],
+      show: false,
       category: {},
       recipe: {},
       ingredients: [],
@@ -53,50 +59,43 @@ new Object({
       style: {
         display: "flex",
         flexFlow: "column",
+        background: "white",
       },
     };
   },
   mounted: async function () {
-    // load category detail
-    this.category = this.$route.query.category;
-    this.recipe = this.$route.query.recipe;
+    // subscribe to data-change event
+    this.event.subscribe(this._uid, "show-recipe", async (event) => {
+      console.log("hey");
+      this.show = true;
+      this.ingredients = null;
+      this.steps = null;
 
-    // configure bread crumbs
-    // Ricette > "Selected Category"
-    this.breadcrumbs.push({
-      text: "Ricette",
-      disabled: false,
-      to: "/",
+      // load category detail
+      this.category = event.category;
+      this.recipe = event.recipe;
+
+      // download ingredients
+      let response = await this.data.get(
+        "Ingredient",
+        `(x) => x.recipe_id == ${this.recipe._id}`
+      );
+      this.ingredients = response.data;
+
+      // download steps
+      response = await this.data.get(
+        "Step",
+        `(x) => x.recipe_id == ${this.recipe._id}`
+      );
+      this.steps = response.data;
     });
-
-    this.breadcrumbs.push({
-      text: this.category.name,
-      disabled: false,
-      to: {
-        path: "/recipe-list",
-        query: {
-          category: this.category,
-        },
-      },
-    });
-
-    this.breadcrumbs.push({
-      text: this.recipe.name,
-      disabled: true,
-    });
-
-    // download ingredients
-    let response = await this.data.get(
-      "Ingredient",
-      `(x) => x.recipe_id == ${this.recipe._id}`
-    );
-    this.ingredients = response.data;
-
-    // download steps
-    response = await this.data.get(
-      "Step",
-      `(x) => x.recipe_id == ${this.recipe._id}`
-    );
-    this.steps = response.data;
+  },
+  destroyed: function () {
+    this.event.unsubscribe_all(this._uid);
+  },
+  methods: {
+    close() {
+      this.show = false;
+    },
   },
 });
--- module/recipe/list/component.js
+++ module/recipe/list/component.js
@@ -2,8 +2,8 @@
 new Object({
   template: `
 <div :style="style">
-  <v-breadcrumbs :items="breadcrumbs"></v-breadcrumbs>
-  <v-parallax v-if="category.image" :src="category.image" height="300">
+  <v-breadcrumbs :items="breadcrumbs" style="position: sticky; top: 0; width: 100%; z-index: 1; background: white;"></v-breadcrumbs>
+  <v-parallax v-if="category.image" :src="category.image" height="200">
     <v-row align="center" justify="center">
       <v-col class="text-center" cols="12" style="background: black; opacity: 0.8;">
         <h1 class="display-1 font-weight-bold mb-4">{{category.name}}</h1>
@@ -11,7 +11,10 @@ new Object({
       </v-col>
     </v-row>
   </v-parallax>
-  <div :style="listStyle">
+  <v-row align="center" justify="center" v-if="!recipes">
+    <v-progress-circular indeterminate color="primary" />
+  </v-row>
+  <div :style="listStyle" v-if="recipes">
     <v-card v-for="(recipe, index) of recipes" :key="index" :style="cardStyle">
       <v-img v-if="recipe.image" height="250" :src="recipe.image"></v-img>
       <v-card-title>{{ recipe.name }}</v-card-title>
@@ -23,6 +26,7 @@ new Object({
       </v-card-actions>
     </v-card>
   </div>
+  <recipe-detail />
 </div>
   `,
   inject: ["data", "config", "event"],
@@ -30,21 +34,23 @@ new Object({
     return {
       breadcrumbs: [],
       category: {},
-      recipes: [],
+      recipes: null,
       style: {
         display: "flex",
         flexFlow: "column",
-        height: "100%"
+        height: "100%",
+        position: "relative"
       },
       listStyle: {
         display: "flex",
         flexFlow: "row wrap",
-        background: "lightgray"
+        background: "lightgray",
+        justifyContent: "center"
       },
       cardStyle: {
         width: "300px",
         maxWidth: "300px",
-        margin: "24px",
+        margin: "12px",
       },
     };
   },
@@ -66,18 +72,19 @@ new Object({
     });
 
     // download recipe lists
-    let response = await this.data.get("Recipe", `(x) => x.category_id == ${this.category._id}`);
+    let response = await this.data.get(
+      "Recipe",
+      `(x) => x.category_id == ${this.category._id}`
+    );
     this.recipes = response.data;
   },
   methods: {
-    click: function(recipe) {
-      this.$router.push({
-        path: `/recipe-detail`,
-        query: {
-          category: this.category,
-          recipe
-        }
-      })
-    }
+    click: function (recipe) {
+      this.event.send({
+        name: "show-recipe",
+        category: this.category,
+        recipe,
+      });
+    },
   },
 });
