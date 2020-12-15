90e9d31c46c0f9e7fdad8000dc2cf7501df0017b
Un Kyu Lee <unkyulee@gmail.com>
2020-12-15 14:50:50 +0100

    image resize

--- backend/src/lib/drive.js
+++ backend/src/lib/drive.js
@@ -1,20 +1,33 @@
 function upload(params) {
-    // get folder 
+  // process file
+  let data = params.file.dataURL;
+  let contentType = data.substring(5, data.indexOf(";"));
+  let bytes = Utilities.base64Decode(data.substr(data.indexOf("base64,") + 7));
+  let blob = Utilities.newBlob(bytes, contentType, params.file.filename);
+
+  let file;
+  if (!params.id) {
+    // create new file
     let folder = DriveApp.getFolderById(params.folder_id);
-    
-    // process file
-    let data = params.file.dataURL;
-    let contentType = data.substring(5, data.indexOf(';'));
-    let bytes = Utilities.base64Decode(data.substr(data.indexOf('base64,') + 7));      
-    let blob = Utilities.newBlob(bytes, contentType, params.file.filename);
+    file = folder.createFile(blob);
+  } else {
+    // update existing file
+    Drive.Files.update(
+      {
+        title: params.file.filename,
+        mimeType: contentType,
+      },
+      params.id,
+      blob      
+    );
+  }
 
-    let file = folder.createFile(blob);
+  // set to public
+  if (params.public)
+    file.setSharing(DriveApp.Access.ANYONE, DriveApp.Permission.VIEW);
 
-    // set to public
-    if(params.public) file.setSharing(DriveApp.Access.ANYONE, DriveApp.Permission.VIEW);    
-
-    return {
-        id: file.getId(),
-        name: file.getName()        
-    }
+  return {
+    id: file.getId(),
+    name: file.getName(),
+  };
 }
--- frontend/src/services/util.service.ts
+++ frontend/src/services/util.service.ts
@@ -9,17 +9,69 @@ export default {
   loadApp() {
     window.app = obj.get(window.params, "parameter.app");
     window.app = this.safeStr(window.app);
-    if (!window.app) window.app = "default";    
+    if (!window.app) window.app = "default";
     console.log(window.app);
   },
   generateId() {
-    return parseInt(`${new Date().getTime() / 100 % 10000000}`);
+    return parseInt(`${(new Date().getTime() / 100) % 10000000}`);
   },
   get(data, path, def) {
     return obj.get(data, path, def);
   },
   set(data, path, value) {
     return obj.set(data, path, value);
-  }
+  },
 
+  resizeImage(file, maxSize) {
+    let reader = new FileReader();
+    let image: any = new Image();
+    let canvas = document.createElement("canvas");
+
+    const dataURItoBlob = (dataURI) => {
+      const bytes = dataURI.split(',')[0].indexOf('base64') >= 0
+        ? atob(dataURI.split(',')[1])
+        : unescape(dataURI.split(',')[1]);
+      const mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
+      const max = bytes.length;
+      const ia = new Uint8Array(max);
+      for (let i = 0; i < max; i += 1) ia[i] = bytes.charCodeAt(i);
+      return new Blob([ia], { type: mime });
+    };    
+
+    const resize = () => {
+      let { width, height } = image;
+
+      if (width > height) {
+        if (width > maxSize) {
+          height *= maxSize / width;
+          width = maxSize;
+        }
+      } else if (height > maxSize) {
+        width *= maxSize / height;
+        height = maxSize;
+      }
+
+      canvas.width = width;
+      canvas.height = height;
+      canvas.getContext("2d").drawImage(image, 0, 0, width, height);
+
+      const dataUrl = canvas.toDataURL("image/jpeg");
+
+      return dataUrl;
+    };
+
+    return new Promise((ok, no) => {
+      if (!file.type.match(/image.*/)) {
+        no(new Error("Not an image"));
+        return;
+      }
+
+      reader.onload = (readerEvent) => {
+        image.onload = () => ok(resize());
+        image.src = readerEvent.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    });
+  },
 };
