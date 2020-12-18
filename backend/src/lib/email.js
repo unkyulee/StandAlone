7063bd97a297078bf9760341a136ceb61805ca97
Un Kyu Lee <unkyulee@gmail.com>
2020-12-18 13:02:37 +0100

    email template

--- backend/src/lib/email.js
+++ backend/src/lib/email.js
@@ -1,4 +1,15 @@
 // send email
 function sendEmail(params) {
+  if(params.data) {
+    // templating subject
+    let subject = HtmlService.createTemplate(params.subject);
+    subject.data = params.data;        
+    params.subject = subject.evaluate().getContent();
+
+    // template body
+    let body = HtmlService.createTemplate(params.body);
+    body.data = params.data;
+    params.body = body.evaluate().getContent();
+  }
   MailApp.sendEmail(params.recipients, params.subject, params.body);
 }
