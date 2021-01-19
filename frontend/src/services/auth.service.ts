79a5c206a88b21a0b1ece077133e2cfc9a958b1c
Un Kyu Lee <unkyulee@gmail.com>
2021-01-19 19:12:22 +0100

    login keeps the session

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -2,50 +2,47 @@
 import data from "./data.service";
 import event from "./event.service";
 
-export default {    
-    isAuthenticated: function() {        
-        return sessionStorage.getItem("auth");
-    },
-    user: function() {
-        let result = {};
-        try {
-            let s = sessionStorage.getItem("auth");
-            result = JSON.parse(s);
-        } catch {
-            sessionStorage.removeItem("auth");
-        }
-
-        return result;
-    },
-    login: async function(_id, password) {
-        // 
-        let response = await data.get(
-            "User", 
-            {
-              filter: {
-                _id: [
-                  {
-                    type: "match",
-                    value: _id
-                  }
-                ],
-                password: [
-                  {
-                    type: "hash",
-                    value: password
-                  }
-                ]
-              },
-              sensitive: {
-                  password: 1
-              }
-          } 
-        );
-        if(response.data && response.data.length > 0) {
-            sessionStorage.setItem("auth", JSON.stringify(response.data[0]));
-            event.send({name: 'login-success'});
-            return true;
-        }
-        return false;
+export default {
+  isAuthenticated: function () {
+    return localStorage.getItem("auth");
+  },
+  user: function () {
+    let result = {};
+    try {
+      let s = localStorage.getItem("auth");
+      result = JSON.parse(s);
+    } catch {
+      localStorage.removeItem("auth");
     }
+
+    return result;
+  },
+  login: async function (_id, password) {
+    //
+    let response = await data.get("User", {
+      filter: {
+        _id: [
+          {
+            type: "match",
+            value: _id,
+          },
+        ],
+        password: [
+          {
+            type: "hash",
+            value: password,
+          },
+        ],
+      },
+      sensitive: {
+        password: 1,
+      },
+    });
+    if (response.data && response.data.length > 0) {
+      localStorage.setItem("auth", JSON.stringify(response.data[0]));
+      event.send({ name: "login-success" });
+      return true;
+    }
+    return false;
+  },
 };
